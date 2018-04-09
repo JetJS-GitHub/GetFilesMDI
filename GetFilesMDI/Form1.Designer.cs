be0739a1d31603bc520992eb4cc1419fef2f74be
Jet Sun <18820095105@139.com>
2018-04-09 15:15:14 +0800

    Office0409

--- GetFilesMDI/Form1.Designer.cs
+++ GetFilesMDI/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace GetFilesMDI
 {
-    partial class Form1
+    partial class MotherForm
     {
         /// <summary>
         /// 必需的设计器变量。
@@ -28,12 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.LbStatus = new System.Windows.Forms.Label();
+            this.btnGetFile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LbStatus
+            // 
+            this.LbStatus.AutoSize = true;
+            this.LbStatus.Location = new System.Drawing.Point(28, 221);
+            this.LbStatus.Name = "LbStatus";
+            this.LbStatus.Size = new System.Drawing.Size(62, 18);
+            this.LbStatus.TabIndex = 0;
+            this.LbStatus.Text = "label1";
+            // 
+            // btnGetFile
+            // 
+            this.btnGetFile.Location = new System.Drawing.Point(709, 449);
+            this.btnGetFile.Name = "btnGetFile";
+            this.btnGetFile.Size = new System.Drawing.Size(201, 66);
+            this.btnGetFile.TabIndex = 1;
+            this.btnGetFile.Text = "Get Files";
+            this.btnGetFile.UseVisualStyleBackColor = true;
+            this.btnGetFile.Click += new System.EventHandler(this.btnGetFile_Click);
+            // 
+            // MotherForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1129, 564);
+            this.Controls.Add(this.btnGetFile);
+            this.Controls.Add(this.LbStatus);
+            this.Name = "MotherForm";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label LbStatus;
+        private System.Windows.Forms.Button btnGetFile;
     }
 }
 
--- GetFilesMDI/Form1.cs
+++ GetFilesMDI/Form1.cs
@@ -7,14 +7,82 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Net;
 
 namespace GetFilesMDI
 {
-    public partial class Form1 : Form
+    public partial class MotherForm : Form
     {
-        public Form1()
+        public MotherForm()
         {
             InitializeComponent();
         }
+
+        private void btnGetFile_Click(object sender, EventArgs e)
+        {
+            if (ReDown_Zcfzb_163() == true)
+            {
+                MessageBox.Show("Download Files Succeed");
+            }else
+	        {
+                MessageBox.Show("Download Files Failed");
+            }
+        }
+
+        private bool ReDown_Zcfzb_163()//通过163把资产负责表文件下载到本地目录，文件名为000002zcfzb.csv
+        {
+            string[] sStockCode = new string[] { "600036", "600282", "000100", "300100" };
+            //string sThisStockDir = "600036";
+            //string sZcfzbName = "600036.csv";
+
+
+            try
+            {
+                for (int i = 0; i < sStockCode.Length; i++)
+                {
+                    string sDownURIHead = "http://quotes.money.163.com/service/zcfzb_";
+                    string sURIMiddle = sStockCode[i];
+                    string sURITail = ".html";
+
+                    string sZcfzbName = sStockCode[i] +".csv";
+                    string sThisStockDir = sStockCode[i];
+
+                    string sDownURI = sDownURIHead + sURIMiddle + sURITail;//得到下载地址。
+
+                    string sDir = System.IO.Path.Combine(Directory.GetCurrentDirectory(), sThisStockDir);
+                    string sFullFileName = System.IO.Path.Combine(sDir, sZcfzbName);
+                    if (!Directory.Exists(sDir))
+                    {
+                        Directory.CreateDirectory(sDir);
+                    }
+                    if (File.Exists(sFullFileName))
+                    {
+                        if (File.Exists(sFullFileName + ".bak"))
+                        {
+                            File.Delete(sFullFileName + ".bak");
+                        }
+                        File.Move(sFullFileName, sFullFileName + ".bak");
+                    }
+                    WebClient Wc = new WebClient();
+                    Application.DoEvents();
+                    Wc.DownloadFile(sDownURI, sFullFileName);
+                    Wc.Dispose();
+                    Application.DoEvents();
+                    LbStatus.Text = sDownURI + " Download to " + sFullFileName;
+                    MessageBox.Show("Download" + sStockCode[i].ToString() + "succeed.");
+                    //SiLogger.AppendLog(sDownURI + " Download to " + sFullFileName);
+                }
+
+            }
+            catch (System.Exception ex)
+            {
+                //SiLogger.AppendLog("_163_DownloadZcfzb Error: " + sStockCode + " " + ex.Message);
+                MessageBox.Show("_163_DownloadZcfzb Error: " + sStockCode + " " + ex.Message);
+                return false;
+            }
+            return true;
+        }
+
     }
 }
